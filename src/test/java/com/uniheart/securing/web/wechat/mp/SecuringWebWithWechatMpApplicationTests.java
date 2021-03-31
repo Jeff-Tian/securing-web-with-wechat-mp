404aacf6abeee621513a86343d8cc430fb8dc6fa
Jeff Tian <jeff.tian@outlook.com>
2021-03-31 19:30:26 +0800

    feat: secured web with basic form

---
+++ src/test/java/com/uniheart/securing/web/wechat/mp/SecuringWebWithWechatMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniheart.securing.web.wechat.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebWithWechatMpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
