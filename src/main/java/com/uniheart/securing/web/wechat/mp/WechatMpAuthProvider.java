f4d7ce17ae4ce07e6d9c34d7ae376834e6c25565
Jeff Tian <jeff.tian@outlook.com>
2021-04-02 17:38:26 +0800

    feat: allow authenticated by WechatMpAuthProvider

--- src/main/java/com/uniheart/securing/web/wechat/mp/WechatMpAuthProvider.java
+++ src/main/java/com/uniheart/securing/web/wechat/mp/WechatMpAuthProvider.java
@@ -1,19 +1,28 @@
 package com.uniheart.securing.web.wechat.mp;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class WechatMpAuthProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("WechatMP"));
+        return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials()
+                , authorities);
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return true;
     }
 }
