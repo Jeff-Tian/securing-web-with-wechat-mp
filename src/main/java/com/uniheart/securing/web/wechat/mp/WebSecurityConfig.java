20da612a20049c811c6e336c28484a0f0ff8a95a
Jeff Tian <jeff.tian@outlook.com>
2021-04-02 17:33:29 +0800

    feat: add WechatMpAuthProvider

--- src/main/java/com/uniheart/securing/web/wechat/mp/WebSecurityConfig.java
+++ src/main/java/com/uniheart/securing/web/wechat/mp/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.uniheart.securing.web.wechat.mp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,6 +30,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+    }
+
+    @Autowired
+    private WechatMpAuthProvider wechatMpAuthProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(wechatMpAuthProvider).userDetailsService(userDetailsService());
     }
 
     @Bean
---
+++ src/main/java/com/uniheart/securing/web/wechat/mp/WechatMpAuthProvider.java
@@ -0,0 +1,19 @@
+package com.uniheart.securing.web.wechat.mp;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WechatMpAuthProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
