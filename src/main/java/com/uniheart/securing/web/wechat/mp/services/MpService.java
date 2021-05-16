4a2a7a16313e949b4e9f90bc58737310e7398947
Jeff Tian <jeff.tian@outlook.com>
2021-05-16 15:01:10 +0800

    feat: add httpclient

--- src/main/java/com/uniheart/securing/web/wechat/mp/services/MpService.java
+++ src/main/java/com/uniheart/securing/web/wechat/mp/services/MpService.java
@@ -3,9 +3,31 @@ package com.uniheart.securing.web.wechat.mp.services;
 import com.uniheart.wechatmpservice.models.MpQR;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @Component
 public class MpService {
+    private final HttpClient httpClient;
+
+    public MpService() {
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
     public MpQR getMpQrCode() {
-        return new MpQR().ticket("test");
+        URI uri = URI.create("https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=TOKEN");
+        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString("")).uri(uri).build();
+
+        try {
+            HttpResponse response = this.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response = " + response.body().toString());
+            return new MpQR().ticket("test");
+        } catch (Exception ex) {
+            System.err.println("Exception = " + ex);
+            ex.printStackTrace();
+            return new MpQR().ticket("test");
+        }
     }
 }
