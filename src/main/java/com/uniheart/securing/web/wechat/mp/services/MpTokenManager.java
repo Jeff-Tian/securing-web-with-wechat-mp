e44dfc796ebb099f3fcef36f1890fc7c40a5e077
Jeff Tian <jeff.tian@outlook.com>
2021-06-08 19:15:11 +0800

    fix: add weixin token endpoint to token manager

--- src/main/java/com/uniheart/securing/web/wechat/mp/services/MpTokenManager.java
+++ src/main/java/com/uniheart/securing/web/wechat/mp/services/MpTokenManager.java
@@ -1,9 +1,16 @@
 package com.uniheart.securing.web.wechat.mp.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MpTokenManager {
+    private final String weixinTokenEndpoint;
+
+    public MpTokenManager(@Value("${weixin-token-endpoint}") String weixinTokenEndpoint){
+        this.weixinTokenEndpoint = weixinTokenEndpoint;
+    }
+
     public WeixinTokenResponse getAccessToken() {
         WeixinTokenResponse res = new WeixinTokenResponse();
 
