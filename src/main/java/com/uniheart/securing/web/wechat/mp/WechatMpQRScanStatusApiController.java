477fa1ba47803ed1db7ebc90f643bbcbeea0efc7
Jeff Tian <jeff.tian@outlook.com>
2021-07-06 14:13:57 +0800

    feat: query scan status

--- src/main/java/com/uniheart/securing/web/wechat/mp/WechatMpQRScanStatusApiController.java
+++ src/main/java/com/uniheart/securing/web/wechat/mp/WechatMpQRScanStatusApiController.java
@@ -1,5 +1,6 @@
 package com.uniheart.securing.web.wechat.mp;
 
+import com.uniheart.securing.web.wechat.mp.services.MpMessageService;
 import com.uniheart.wechatmpservice.api.MpQrScanStatusApi;
 import com.uniheart.wechatmpservice.models.MpQRScanStatus;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public final class WechatMpQRScanStatusApiController implements MpQrScanStatusApi {
+    private final MpMessageService mpMessageService;
+
+    public WechatMpQRScanStatusApiController(MpMessageService mpMessageService) {
+        this.mpMessageService = mpMessageService;
+    }
+
     @Override
     public ResponseEntity<MpQRScanStatus> mpQrScanStatus(String ticket) {
         return new ResponseEntity<>(new MpQRScanStatus().openId("1234"), HttpStatus.OK);
